438195165d0248ed1ad9cc8eef4af31d313a998b
curiosity <chengchaos@gmail.com>
2015-11-16 20:22:05 +0800

    Update WebjarsServlet.java

--- src/main/java/org/webjars/servlet/WebjarsServlet.java
+++ src/main/java/org/webjars/servlet/WebjarsServlet.java
@@ -1,16 +1,15 @@
 package org.webjars.servlet;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * <p>This servlet enables Servlet 2.x compliant containers to serve up Webjars resources</p>
