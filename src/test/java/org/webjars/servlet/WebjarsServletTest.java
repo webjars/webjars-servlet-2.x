ebb88647a967ca2cdc8aa5e1605bb984656fe51b
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-12-05 15:44:12 +0000

    Update to latest servlet api.

--- src/test/java/org/webjars/servlet/WebjarsServletTest.java
+++ src/test/java/org/webjars/servlet/WebjarsServletTest.java
@@ -7,11 +7,11 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
